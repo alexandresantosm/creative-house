cf843609a60b3990014e44a0bdb5b405ec1afd7c
Alexandre Santos <amenezes031@gmail.com>
2022-07-15 20:19:51 -0300

    :sparkles: feat: create server

--- server.js
+++ server.js
@@ -1,1 +1,13 @@
-console.log("Hello World!");
+import express from 'express';
+
+const PORT_SERVER = 3000;
+
+const server = express();
+
+server.get("/", (req, res) => {
+  return res.send('<h1>Hello World!</h1>')
+});
+
+server.listen(PORT_SERVER, () => {
+  console.log(`Server is running in http://localhost:${PORT_SERVER}/`)
+});
