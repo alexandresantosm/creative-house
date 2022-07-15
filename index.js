8c80cf3eb969a774f51a6458956f884daab36f09
Alexandre Santos <amenezes031@gmail.com>
2022-07-14 21:01:41 -0300

    :bug: fix: corrections in modal

--- index.js
+++ index.js
@@ -3,4 +3,14 @@ function onOff() {
     .querySelector('#modal')
     .classList
     .toggle('hidden');
+
+  document
+    .querySelector('body')
+    .classList
+    .toggle('hiddenScroll');
+
+  document
+    .querySelector('#modal')
+    .classList
+    .toggle('addScroll');
 }
