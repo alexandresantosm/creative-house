77ade8bccb0236d26d3b47f7ab6ba6e9d1dd6cdd
Alexandre Santos <amenezes031@gmail.com>
2022-07-18 19:36:48 -0300

    :recycle: refactor: get ideas from database

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import nunjucks from 'nunjucks';
-import { ideas } from './mocks/ideas-mock.js';
+import { database } from './database/config/index.js';
 
 const PORT_SERVER = 3000;
 
@@ -13,17 +13,71 @@ nunjucks.configure('views', {
   noCache: true,
 });
 
-const ideasOrderByLast = [...ideas]
+const ideasOrderByLast = (ideas) => [...ideas]
   .reverse();
 
 server.get("/", (req, res) => {
-  const lastIdeas = ideasOrderByLast.slice(0, 3);
+  const querySelectAllIdeas = `SELECT * FROM ideas`;
+  
+  database.all(querySelectAllIdeas, (err, rows) => {
+    let ideas = [];
 
-  return res.render('index.html', { ideas: lastIdeas });
+    if (err) {
+      console.log(err);
+
+      return res.render('error.html');
+    }
+
+    rows.forEach((row) => {
+      let idea = {
+        id: row.id,
+        imageUrl: row.image_url,
+        imageDescription: row.image_description,
+        title: row.title,
+        category: row.category,
+        description: row.description,
+        ideaUrl: row.idea_url
+      }
+
+      ideas = [...ideas, idea];
+    });
+    
+    const lastIdeas = ideasOrderByLast(ideas).slice(0, 3);
+  
+    return res.render('index.html', { ideas: lastIdeas });
+  });
 });
 
 server.get("/ideas", (req, res) => {
-  return res.render('ideas.html', { ideas: ideasOrderByLast });
+  const querySelectAllIdeas = `SELECT * FROM ideas`;
+  
+  database.all(querySelectAllIdeas, (err, rows) => {
+    let ideas = [];
+
+    if (err) {
+      console.log(err);
+
+      return res.render('error.html');
+    }
+
+    rows.forEach((row) => {
+      let idea = {
+        id: row.id,
+        imageUrl: row.image_url,
+        imageDescription: row.image_description,
+        title: row.title,
+        category: row.category,
+        description: row.description,
+        ideaUrl: row.idea_url
+      }
+
+      ideas = [...ideas, idea];
+    });
+    
+    const allIdeas = ideasOrderByLast(ideas);
+  
+    return res.render('ideas.html', { ideas: allIdeas });
+  });
 });
 
 server.listen(PORT_SERVER, () => {
