d1952a81a13fa7c31384d3c0062ffd8dee211fc4
Alexandre Santos <amenezes031@gmail.com>
2022-07-18 22:34:21 -0300

    :sparkles: feat: handling empty field errors

--- public/index.js
+++ public/index.js
@@ -14,3 +14,43 @@ function onOff() {
     .classList
     .toggle('addScroll');
 }
+
+function checkFields(event) {
+  const valuesToCheck = [
+    "title",
+    "category",
+    "image-url",
+    "description",
+    "idea-url",
+  ];
+
+  const valuesFields = [
+    {"title": "Título da ideia"},
+    {"category": "Categoria"},
+    {"image-url": "Link da imagem"},
+    {"description": "Digite uma descrição para essa ideia"},
+    {"idea-url": "Link da ideia"},
+  ];
+
+  const isEmpty = valuesToCheck.find((value) => {
+    const checkIfIsString = typeof event.target[value].value === 'string';
+    const checkIfIsEmpty = !event.target[value].value.trim();
+
+    if (checkIfIsString && checkIfIsEmpty) {
+      return true;
+    }
+  });
+
+  if (isEmpty) {
+    event.preventDefault();
+
+    const field = valuesFields.find((item) => item[isEmpty]);
+
+    swal({
+      title: "Valor inválido!",
+      text: `Por favor preencha o campo "${field[isEmpty]}".`,
+      icon: "warning",
+      dangerMode: true,
+    });
+  }
+}
